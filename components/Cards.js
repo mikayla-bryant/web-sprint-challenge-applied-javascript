f24a4cae1945294992950a084a864cd6db3f3fd7
mikayla-bryant <mikaylabryant25@gmail.com>
2020-12-04 14:03:50 -0500

    Printed out Article Arrays

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,62 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const entryPointCards = document.querySelector('.cards-container');
+
+const cardCreator = () => {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const authContainer = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const author = document.createElement('span');
+
+    card.appendChild(headline);
+    card.appendChild(authContainer);
+    authContainer.appendChild(imageContainer);
+    imageContainer.appendChild(image);
+    authContainer.appendChild(author);
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    authContainer.classList.add('author');
+    imageContainer.classList.add('img-container');
+    
+
+    headline.textContent = "Headline";
+    //image.src = article.authorPhoto;
+    author.textContent = "Test text";
+
+
+    return card;
+}
+const result = cardCreator();
+entryPointCards.appendChild(result);
+
+
+const axiosPromise = axios.get('https://lambda-times-api.herokuapp.com/articles')
+.then(response => {
+    for(let i = 0; i < response.data.articles.javascript.length; i++){
+        const array = response.data.articles.javascript[i];
+        console.log(array);
+    }
+  
+})
+
+/*const axiosPromise = axios.get('https://lambda-times-api.herokuapp.com/articles')
+.then(response => {
+    for(let i = 0; i < response.data.articles.javascript.length; i++){
+        console.log(response.data.articles.javascript[i]); //response.data.articles.javascript[array]
+    }
+  
+})
+ I got this to print each object in the array
+*/ 
+
+/*
+const axiosPromise = axios.get('https://lambda-times-api.herokuapp.com/articles')
+.then( response => {
+        console.log("success", response);
+    })
+*/
